02868b29ad5fbd43d1a234fb61afe5879fd1e1ba
Tarcisio Corte <tarcisiocorte@gmail.com>
2022-03-17 18:29:27 -0300

    boolean

--- test.go
+++ test.go
@@ -5,11 +5,16 @@ package main
 import "fmt"
 
 func main() {
-	var a complex128 = complex(6, 2)
-	var b complex64 = complex(9, 2)
-	fmt.Println(a)
-	fmt.Println(b)
+	str1 := "AAA"
+	str2 := "aaa"
+	str3 := "aAa"
+	result1 := str1 == str2
+	result2 := str2 == str3
 
-	fmt.Printf("The type of a is %T and "+
-		"the type of b is %T", a, b)
+	fmt.Println(result1)
+	fmt.Println(result2)
+
+	fmt.Printf("The type of result1 is %T and "+
+		"the type of result2 is %T",
+		result1, result2)
 }
