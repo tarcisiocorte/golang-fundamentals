65fc89241a028e52555f43cca2b88f90eebd1559
Tarcisio Corte <tarcisiocorte@gmail.com>
2022-03-17 19:00:16 -0300

    illustrate the concept of variable

--- test.go
+++ test.go
@@ -5,8 +5,23 @@ package main
 import "fmt"
 
 func main() {
-	var var1, var2, var3 int = 10, 20, 30
-	fmt.Printf("The value of myvariable1 is : %d\n", var1)
-	fmt.Printf("The value of myvariable2 is : %d\n", var2)
-	fmt.Printf("The value of myvariable3 is : %d", var3)
+	var myvariable1, myvariable2, myvariable3 = 2, "GFG", 67.56
+
+	fmt.Printf("The value of myvariable1 is : %d\n",
+		myvariable1)
+
+	fmt.Printf("The type of myvariable1 is : %T\n",
+		myvariable1)
+
+	fmt.Printf("\nThe value of myvariable2 is : %s\n",
+		myvariable2)
+
+	fmt.Printf("The type of myvariable2 is : %T\n",
+		myvariable2)
+
+	fmt.Printf("\nThe value of myvariable3 is : %f\n",
+		myvariable3)
+
+	fmt.Printf("The type of myvariable3 is : %T\n",
+		myvariable3)
 }
