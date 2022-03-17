8b550547cfb0572952e799a27fa91cbfc90b6527
Tarcisio Corte <tarcisiocorte@gmail.com>
2022-03-17 17:54:01 -0300

    use of integers

--- test.go
+++ test.go
@@ -1,4 +1,3 @@
-
 // You can edit this code!
 // Click here and start typing.
 package main
@@ -6,5 +5,9 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("Hello, 世界")
+	var x uint8 = 225
+	fmt.Println(x, x-3)
+
+	var y int16 = 32767
+	fmt.Println(y+2, y-2)
 }
