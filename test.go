a37edbd51c49119872e4bca263e8982e8c1a22b1
Tarcisio Corte <tarcisiocorte@gmail.com>
2022-03-17 18:51:14 -0300

    variable declared and initialized without expression

--- test.go
+++ test.go
@@ -5,26 +5,17 @@ package main
 import "fmt"
 
 func main() {
-
-	var myvariable1 = 20
-	var myvariable2 = "AaBb"
-	var myvariable3 = 34.80
+	var myvariable1 int
+	var myvariable2 string
+	var myvariable3 float64
 
 	fmt.Printf("The value of myvariable1 is : %d\n",
-		myvariable1)
-
-	fmt.Printf("The type of myvariable1 is : %T\n",
 		myvariable1)
 
 	fmt.Printf("The value of myvariable2 is : %s\n",
 		myvariable2)
 
-	fmt.Printf("The type of myvariable2 is : %T\n",
-		myvariable2)
-
-	fmt.Printf("The value of myvariable3 is : %f\n",
+	fmt.Printf("The value of myvariable3 is : %f",
 		myvariable3)
 
-	fmt.Printf("The type of myvariable3 is : %T\n",
-		myvariable3)
 }
