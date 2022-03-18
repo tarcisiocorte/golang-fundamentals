34af99b11dbbd5d67c4ba7458db9741dbdce3a98
Tarcisio Corte <tarcisiocorte@gmail.com>
2022-03-18 05:17:21 -0300

    illustrate use of constants

--- test.go
+++ test.go
@@ -5,17 +5,8 @@ package main
 import "fmt"
 
 func main() {
-
-	myvar1 := 39
-	myvar2 := "AbCd"
-	myvar3 := 34.67
-
-	fmt.Printf("The value of myvar1 is : %d\n", myvar1)
-	fmt.Printf("The type of myvar1 is : %T\n", myvar1)
-
-	fmt.Printf("\nThe value of myvar2 is : %s\n", myvar2)
-	fmt.Printf("The type of myvar2 is : %T\n", myvar2)
-
-	fmt.Printf("\nThe value of myvar3 is : %f\n", myvar3)
-	fmt.Printf("The type of myvar3 is : %T\n", myvar3)
+	const Correct = true
+	fmt.Println("Life is good: ", Correct)
+	const TEXT = "a small wording"
+	fmt.Println("See this text: ", TEXT)
 }
