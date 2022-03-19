05c89779ed2a739afad8b0e4c67566a326e733a2
Tarcisio Corte <tarcisiocorte@gmail.com>
2022-03-19 10:44:46 -0300

    Coding - Types and Zero Values

--- test.go
+++ test.go
@@ -4,47 +4,14 @@ import "fmt"
 
 func main() {
 
-	var s1 string
-	s1 = "Learning Go!"
-	fmt.Println(s1)
+	var a int = 10
+	var b = 15.5
+	c := "Gopher"
+	_, _, _ = a, b, c
 
-	var k int = 6
-	var i = 5
-	var j = 5.6
-
-	fmt.Println("i:", i, "j:", j, "k:", k)
-
-	var s2 = "Go!"
-	_ = s2
-
-	var ii, jj int
-	ii, jj = 5, 8
-
-	ii, jj = jj, ii
-
-	fmt.Println(ii, jj)
-
-	s3 := "Learning golang!"
-	_ = s3
-
-	car, cost := "Audi", 50000
-	fmt.Println(car, cost)
-
-	var deleted = false
-	deleted, file := true, "a.txt"
-	_, _ = deleted, file
-
-	sum := 5 + 2.5
-	fmt.Println(sum)
-
-	var (
-		age       float64
-		firstName string
-		gender    bool
-	)
-	_, _, _ = age, firstName, gender
-
-	var a, b int
-	_, _ = a, b
-
+	var value int
+	var price float64
+	var name string
+	var done bool
+	fmt.Println(value, price, name, done)
 }
